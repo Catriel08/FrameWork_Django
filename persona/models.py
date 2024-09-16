1be29fe18bb19a969f9405716f3d749a8e26b096
Brayan Grimaldo <brayangrimaldo2004@gmail.com>
2024-09-16 07:24:12 -0500

    Se agregan las app curso y persona, configuración de configuración de modelos en la app persona

---
+++ persona/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Persona (models.Model):
+    nombre=models.CharField(max_length=100)
+    apellidos=models.CharField(max_length=100)
+    dni=models.CharField(max_length=10)
+    telefono=models.CharField(max_length=10)
+    email=models.EmailField()
+    fecha_nacimiento=models.DateField()
+    rol=models.CharField(max_length=20)
+    create_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f'{self.nombre} {self.apellidos} - {self.dni}'
+    
+class Meta:
+        db_table='persona'
